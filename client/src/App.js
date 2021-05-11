87592d8e4ded3f603acedfecccdc9915acafa7e8
David Bueno <davidbuenox8@gmail.com>
2021-05-11 14:43:13 +0200

    added authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,73 @@
 
 import './App.css';
+import { Route, Redirect } from 'react-router-dom';
 import Projects from './components/Projects';
-import Navbar from './components/Navbar';
-import { Route } from 'react-router-dom';
+import React from 'react';
+import Signup from './components/Signup';
 import ProjectDetails from './components/ProjectDetails';
+import Navbar from './components/Navbar';
+import Login from './components/Login';
+import ProtectedRoute from './components/ProtectedRoute';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
 
-      <Navbar />
-      <Route exact path="/projects" component={Projects} />
-      <Route exact path="/projects/:id" component={ProjectDetails} />
+  state = {
+    user: this.props.user
+  }
 
+  setUser = user => {
+    this.setState({ user });
+  }
 
-    </div>
-  );
+  render() {
+    return (
+      <div className="App" >
+        <Navbar user={this.state.user} setUser={this.setUser} />
+
+        {/* <Route exact path="/projects" component={Projects} /> 
+         The above route should now be protected -> if there is no logged in user
+        in the state then we want to redirect to '/login' - otherwise render the Projects 
+        component */}
+        {/*    <Route
+          exact path='/projects'
+          render={props => {
+            if (this.state.user) {
+              return <Projects {...props} />
+              //this is the Redirect component from react router
+            } else return <Redirect to='/login'
+            />
+          }}
+        /> */}
+
+        <ProtectedRoute
+          path='/projects'
+          user={this.state.setUser}
+          component={Projects}
+          redirectPath='/login'
+        />
+
+        <Route exact path="/projects/:id" component={ProjectDetails} />
+        <Route
+          exact path="/signup"
+          // component={Signup}
+          // we need to use the render prop to be able to pass props to the Signup component
+          // and for this we need to be able to reference the component like this: <Signup />
+          // https://reactjs.org/docs/render-props.html
+          render={props => <Signup setUser={this.setUser} {...props} />}
+        />
+        {/* if you don't want to use the render prop but still need to pass props to the 
+         component referenced in the route
+         <Route exact path="/login">
+          <Login setUser={this.setUser} {...this.props} />
+        </Route> */}
+        <Route
+          exact path="/login"
+          render={props => <Login setUser={this.setUser} {...props} />}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
+
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,15 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { logout } from '../services/auth';
 
-export default function Navbar() {
+export default function Navbar(props) {
+  const handleLogout = () => {
+    logout().then(() => {
+      props.setUser(null);
+    })
+  }
   return (
     <ul>
       <li>
         <Link to="/">Home</Link>
       </li>
-      <li>
-        <Link to="/projects">Projects</Link>
-      </li>
+      {props.user ? (
+        <>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link to='/' onClick={() => handleLogout()}>Logout</Link>
+          </li>
+        </>
+      ) : (
+        <>
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+          <li>
+            <Link to="/login">Log In</Link>
+          </li>
+        </>
+      )}
     </ul>
   )
 }
